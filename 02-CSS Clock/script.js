d283808fa2aad86e61d938367f5751351f3ec71a
keff6 <kev.fallas@gmail.com>
2018-01-30 15:20:24 -0600

    CSS Clock initial commit / Drum Kit folder rename

---
+++ 02-CSS Clock/script.js
@@ -0,0 +1,26 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+
+function setDate(){
+  const now = new Date();
+  
+  // Seconds
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  
+  // Minutes
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  
+  // Hours
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  
+}
+
+setInterval(setDate,1000);
